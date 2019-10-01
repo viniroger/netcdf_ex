99f6628219721c770d2eb660eb22d1daa65f88f1
Vinicius Roggério da Rocha <viniroger@yahoo.com.br>
2019-10-01 08:26:07 -0300

    Include format option and changed variable format

--- write_netcdf.py
+++ write_netcdf.py
@@ -7,7 +7,7 @@ n2 = 538
 #data_array = np.zeros((n1, n2))
 data_array = np.random.randint(400, size=(n1, n2))
 
-# Creating dataset
+# Creating dataset; another format option 'NETCDF3_CLASSIC'
 dataset = Dataset('dataset.nc', 'w', format='NETCDF4')
 
 # Creating the dimensions to use
@@ -15,7 +15,7 @@ dataset.createDimension('x', n1)
 dataset.createDimension('y', n2)
 
 # Creating variable (name, format, dimensions)
-var = dataset.createVariable('varname', 'f8', ('x', 'y'))
+var = dataset.createVariable('varname', 'float32', ('x', 'y'))
 
 # Writing data
 dataset['varname'][:] = data_array
